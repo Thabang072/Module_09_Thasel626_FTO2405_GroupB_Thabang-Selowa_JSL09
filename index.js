26ea3642388126619d9c44a5021689d0ef163717
Thabang Selowa <mohale20cobra@gmail.com>
2024-08-11 20:22:49 +0200

    Add function for data crypto-top and crypto

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 		document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
-    // Get API for 
+    // Get data for crypyo-top
 fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     .then(res => {
         if (!res.ok) {
@@ -37,3 +37,23 @@ function getCurrentTime() {
 }
 // Add callback function to setinterval for currentTime
 setInterval(getCurrentTime, 1000)
+
+// Add currentPosition data
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
+            `
+        })
+        .catch(err => console.error(err))
+});
